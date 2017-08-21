f581cd99404da98b5edad4e182808fd3d65211b0
liangchangyou <liangchangyou@youmi.net>
2017-08-21 09:28:04 +0800

    json

---
+++ remove_3till5char.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"io"
+)
+
+func main() {
+	inputFile, _ := os.Open("fib.go")
+	outputFile, _ := os.OpenFile("goprogramT", os.O_WRONLY|os.O_CREATE, 0666)
+	defer inputFile.Close()
+	defer outputFile.Close()
+	inputReader := bufio.NewReader(inputFile)
+	outputWriter := bufio.NewWriter(outputFile)
+	for {
+		inputString, _, readerError := inputReader.ReadLine()
+		if readerError == io.EOF {
+			fmt.Println("EOF")
+			return
+		}
+		outputString := string(inputString[2:5]) + "\r\n"
+		_, err := outputWriter.WriteString(outputString)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	fmt.Println("Conversion done")
+}
